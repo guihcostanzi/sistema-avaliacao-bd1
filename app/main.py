deb952e6e5eff0ce2371aa1693a41e7102b6a878
Guilherme Costanzi Do <guilherme.costanzi@genesisgroup.com.br>
2025-10-16 16:51:02 -0300

    Adicionando funcionalidade no middleware para definir páginas públicas, e redirecionamento automático para o login.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from fastapi import Depends
 
+# Importa a instância centralizada do motor de templates
+from app.templating import templates as templates_instance
+ 
 # Importa o middleware de sessão
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -20,27 +22,52 @@ app = FastAPI(
     version="0.2.0",
 )
 
+@app.middleware("http")
+async def redirecionar_se_nao_autenticado(request: Request, call_next):
+   
+    # Define uma lista de caminhos que NÃO precisam de autenticação.
+    allowed_paths = [
+        "api/auth/login",
+        "/docs",
+        "/openapi.json"
+    ]
+    
+    # Não bloqueia requisições para arquivos estáticos ou rotas de API
+    if request.url.path.startswith("/static") or request.url.path.startswith("/api"):
+        response = await call_next(request)
+        return response
+
+    usuario = request.session.get('usuario')
+
+    # Se o usuário NÃO está logado E o caminho que ele tenta acessar NÃO está na lista de permitidos, redireciona para a página de login.
+    if not usuario and request.url.path not in allowed_paths:
+        return RedirectResponse(url="/api/auth/login")
+
+    response = await call_next(request)
+    return response
+
 # Adiciona o middleware de sessão ao aplicativo
 app.add_middleware(
     SessionMiddleware,
     secret_key="xK9mP2vL8qR5nW7jE_______45>opse<22______fG9hJ2kL5nM8qR1wE4rT7yU0iO3pA6sD9f"
 )
 
+
 # Configuração de arquivos estáticos e templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates = templates_instance
 
 # --- Incluir os Roteadores ---
 
 app.include_router(
     authentication.router, 
-    prefix="",
+    prefix="/api/auth",
     tags=["Autenticação"]   # Agrupa estas rotas na documentação /docs
 )
 
 app.include_router(
     usuarios.router, 
-    prefix="/usuarios",        
+    prefix="/api/usuarios",        
     tags=["Usuarios"]   
 )
 
